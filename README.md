0eeac27658467492ec918006bc3013cbb21510d3
Felix Leblanc <felix.leblanc1305@gmail.com>
2020-05-08 20:57:57 -0400

    documentation

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ python convert_to_maze.py encoded.txt
 ```
 
 Example: You can use the `test_encoded_maze.txt` file to test the converter
+
+### Symbols
+- 0: Wall
+- 255: Floor
+- 74: blue
+- 80: red
+- 1: start
+- 2: end
--- convert_to_maze.py
+++ convert_to_maze.py
@@ -5,6 +5,9 @@ import numpy as np
 import sys
 
 def arrow_symbol(block):
+    """
+    Check the direction of the arrow in the block and return the appropriate symbol
+    """
     diff_num_black_pixels = np.apply_along_axis(lambda x: np.bincount(x)[0], 1, block)
     for i in range(len(diff_num_black_pixels) - 1):
         if diff_num_black_pixels[i] < diff_num_black_pixels[i + 1]:
@@ -15,6 +18,9 @@ def arrow_symbol(block):
     return np.argmax(np.bincount(block.flatten()))
 
 def to_symbol(block, last_line=False):
+    """
+    Convert the matrix of pixels into a single symbol
+    """
     counts = np.bincount(block.flatten())
     if last_line:
         return arrow_symbol(block)
@@ -26,6 +32,9 @@ def to_symbol(block, last_line=False):
         return np.argmax(counts)
 
 def convert_to_maze(data):
+    """
+    Convert a numpy array representing an image of a maze into a matrix of symbols
+    """
     starting_coordinates = (65, 195)
     start_y = 50
     start_x = 195
@@ -36,13 +45,14 @@ def convert_to_maze(data):
     bottom_padding = 45
     right_padding = 55
 
+    # Cropping the image to only contain the maze
     maze = data[start_x:data.shape[0]-bottom_padding, start_y:data.shape[1]-right_padding]
+
+    # Divide the maze's image matrix into smaller matrices and find the symbol for each sub-matrix
     maze_width = maze.shape[1]
     maze_height = maze.shape[0]
-
     num_blocks_x = int(maze_height / win_height)
     num_blocks_y = int(maze_width / win_width)
-
     start_x = 0
     start_y = 0
     converted_maze = np.empty((num_blocks_x, num_blocks_y))
